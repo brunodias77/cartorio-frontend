90170722c058e1cb38d6c4994b2007a9953a95d8
brunodias77 <brunohdias95@gmail.com>
2026-01-11 01:27:13 -0300

    ...

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://vps54701.publiccloud.com.br/api";
+  import.meta.env.VITE_API_URL || "https://vps54701.publiccloud.com.br/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
